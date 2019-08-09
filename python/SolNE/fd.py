8d4251039e5562b7d603caea117b8f3c4c923938
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-09 14:37:50 -0600

    Packages: Update

---
+++ python/SolNE/fd.py
@@ -0,0 +1,17 @@
+def sne_fd_1(*args, **kwargs):
+    pass
+
+def sne_fd_2(*args, **kwargs):
+    pass
+
+def sne_fd_3(*args, **kwargs):
+    pass
+
+def sne_fd_4(*args, **kwargs):
+    pass
+
+def sne_fd_5(*args, **kwargs):
+    pass
+
+def sne_fd_6(*args, **kwargs):
+    pass
--- python/SolNE/ud.py
+++ python/SolNE/ud.py
@@ -1,17 +1,17 @@
-def sne_ud_1(**args, **kwargs):
-    pass;
+def sne_ud_1(*args, **kwargs):
+    pass
 
-def sne_ud_2(**args, **kwargs):
-    pass;
+def sne_ud_2(*args, **kwargs):
+    pass
 
-def sne_ud_3(**args, **kwargs):
-    pass;
+def sne_ud_3(*args, **kwargs):
+    pass
 
-def sne_ud_4(**args, **kwargs):
-    pass;
+def sne_ud_4(*args, **kwargs):
+    pass
 
-def sne_ud_5(**args, **kwargs):
-    pass;
+def sne_ud_5(*args, **kwargs):
+    pass
 
-def sne_ud_6(**args, **kwargs):
-    pass;
+def sne_ud_6(*args, **kwargs):
+    pass

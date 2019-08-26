5076c638e3a87a054ad340b4549c63a5974e45b3
Erick Obregón Fonseca <erickobregonf@gmail.com>
2019-08-25 19:09:33 -0600

    Python: setup.py

---
+++ python/SolNE/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='SolNe',
+    version='1.0',
+    description='A package for solving nonlinear equations',
+    url='#',
+    license='GPLv3+',
+    packages=[],
+    zip_safe=False)
